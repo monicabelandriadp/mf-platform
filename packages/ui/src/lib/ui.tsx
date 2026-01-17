7f89120de1cfbb26d51ef81c90ca8dd4baefaa2f
Monica Belandria <monica.belandria@ext.konecta-group.com>
2026-01-17 12:21:55 -0300

    creacion del proyecto UI con vite (vacio)

---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function PlatformUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to PlatformUi!</h1>
+    </div>
+  );
+}
+
+export default PlatformUi;
