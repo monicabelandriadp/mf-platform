baf6151bfc5a42565eeaf6af66d137966fcfd26f
Monica Belandria <monica.belandria@ext.konecta-group.com>
2026-01-17 01:49:40 -0300

    creacion del proyecto logi (remote)

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
 
+const Login = React.lazy(() => import('login/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -9,9 +11,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </React.Suspense>
   );
